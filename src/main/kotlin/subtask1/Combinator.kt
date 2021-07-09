c6d6ff28482bafffeb803362993b291b28b92144
Mits16 <mits1388@gmail.com>
2021-07-09 14:34:31 +0300

    fix: code

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,27 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        val posters = array[0]
+        val colors = array[1]
+        var result: Int? = null
+
+        for (i in 1..colors){
+            if(posters <= factorial(colors)/(factorial(colors-i)*factorial(i))){
+                result = i
+                    break
+                }
+        }
+        return result
     }
+
+    private fun factorial(number: Int): Double{
+      var result = 1.0
+      if (number != 0) {
+          for (i in 1..number){
+              result *= i
+          }
+      }
+      return result
+  }
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -1,9 +1,38 @@
 package subtask3
 
+
 class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+
+        if (number.toInt() < 0) return null
+
+        val result = arrayListOf<String>()
+
+        number.forEachIndexed { index, c ->
+            neighborsNumbers(c).forEach {
+                result.add(number.substring(0,index)+it+number.substring(index+1))
+            }
+
+        }
+        return result.toTypedArray()
+
     }
+
+    fun neighborsNumbers(number: Char) = when (number) {
+            '0' -> arrayOf('8')
+            '1' -> arrayOf('2', '4')
+            '2' -> arrayOf('1', '3', '5')
+            '3' -> arrayOf('2', '6')
+            '4' -> arrayOf('1', '5', '7')
+            '5' -> arrayOf('2', '4', '6', '8')
+            '6' -> arrayOf('3', '5', '9')
+            '7' -> arrayOf('4', '8')
+            '8' -> arrayOf('5', '7', '9', '0')
+            '9' -> arrayOf('6', '8')
+            else -> arrayOf()
+        }
+
+
 }
