0b6088cb77a99782094f004057d8d1eb1cc21328
Mits16 <mits1388@gmail.com>
2021-07-13 19:15:26 +0300

    fix: square

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,41 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+    val square = Math.pow(number.toDouble(), 2.0)
+        val list = decompose(square, number -1 )
+        return list?.toTypedArray()
+    }
+
+         private fun decompose(square: Double, position: Int): ArrayList<Int>?{
+
+
+             for(i in position downTo 1){
+
+                 val squareI = Math.pow(i.toDouble(), 2.0)
+                     if(square - squareI == 0.0){
+                     return arrayListOf(i)
+                 }
+                 if (square - squareI < 0) {
+                     return null
+                 }
+
+                 var positionNew = sqrt(square - squareI).toInt()
+                 if (positionNew >= i) {
+                     positionNew = i - 1
+                 }
+
+                 val result = decompose(square - squareI, positionNew)
+                 if (result != null) {
+                     result.add(i)
+                     return result
+                 }
+
+             }
+             return null
     }
 }
